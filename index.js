00d7d905c62431926892f87a3240eb84cc0d9259
Daniel Sellers <d.sellers@ldschurch.org>
2014-12-10 12:09:26 -0700

    add the feedback from the code review and working on breaking change release

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //TODO: have the cache drop expired items on an interval of some sort
+//TODO: memory size?
+//TODO: jshint and editorconfig (add them yo)
+
 var cache = {}
-    , isExpired = function(key){
+    , isExpired = function (key) {
         var currentTime = new Date()
             , expired = !!(currentTime.getTime() >= cache[key].expires.getTime() || cache[key].expires === 0);
 
@@ -10,10 +13,9 @@ var cache = {}
         }
 
         return expired;
-    };
+    }
 
-module.exports = {
-    getFromCache: function (key) {
+    , getFromCache = function (key) {
         if(typeof cache[key] !== 'undefined' && !isExpired(key)){
             return cache[key].data;
         } else {
@@ -21,7 +23,29 @@ module.exports = {
         }
     }
 
-    , addToCache: function (key, object, expires) {
+    , cacheContains = function () {
+        var obj = {};
+
+        Object.getOwnPropertyNames(cache).forEach(function(item){
+            obj[item] = cache[item].expires;
+        });
+
+        return obj;
+    }
+
+    , clearCache = function () {
+        cache = {};
+    }
+
+    , removeFromCache = function (key) {
+        try{
+            delete cache[key];
+        } catch (e) {
+            //an error...
+        }
+    }
+
+    , addToCache = function (key, object, expires) {
         cache[key] = {
             data: object
             , expires: new Date(new Date().getTime() + expires)
@@ -31,5 +55,12 @@ module.exports = {
         if(expires === 0){
             cache[key].expires = 0;
         }
-    }
-}
+    };
+
+module.exports = {
+    get: getFromCache
+    , clear: clearCache
+    , remove: removeFromCache
+    , contains: cacheContains
+    , add: addToCache
+};
--- index_test.js
+++ index_test.js
@@ -4,12 +4,12 @@ var assert = require('chai').assert
 describe('lds-node-cache tests', function(){
 
     describe('should be defined correctly', function(){
-        it('should have a getFromCache function', function(){
-            assert.isFunction(cache.getFromCache);
+        it('should have a get function', function(){
+            assert.isFunction(cache.get);
         });
 
-        it('should have an addToCache function', function(){
-            assert.isFunction(cache.addToCache);
+        it('should have an add function', function(){
+            assert.isFunction(cache.add);
         });
     });
 
@@ -19,5 +19,16 @@ describe('lds-node-cache tests', function(){
         it('should not return expired items from the cache');
         it('should allow permenant caching of items');
         it('should remove expired items from the cache');
+    });
+
+    describe('should be able to find out what is in the cache now', function(){
+        it('should return an object of keys with time that the key expires');
+    });
+
+    describe('should allow items to be manually removed from the cache', function(){
+        it('should have a clear function');
+        it('should clear the entire cache with clear')
+        it('should have a remove function that takes a key');
+        it('should remove an item from cache by key');
     });
 });
